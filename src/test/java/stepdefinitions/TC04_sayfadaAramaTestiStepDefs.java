044cb343094300f7e0b8104250b96cc9d1a6e2d7
Göksel Celik <gokselcelik5255@gmail.com>
2023-11-29 00:53:23 +0300

    29.11.2023

---
+++ src/test/java/stepdefinitions/TC04_sayfadaAramaTestiStepDefs.java
@@ -0,0 +1,18 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.And;
+import pages.HomePage;
+
+public class TC04_sayfadaAramaTestiStepDefs {
+    HomePage homePage=new HomePage();
+    @And("kullanici arama alanina tiklar")
+    public void kullaniciAramaAlaninaTiklar() throws InterruptedException {
+        homePage.searchBoxHome.click();
+        Thread.sleep(2000);
+    }
+
+    @And("kullanici arama alanina en az iki karakter girmesi gerektigini gorur")
+    public void kullaniciAramaAlaninaEnAzIkiKarakterGirmesiGerektiginiGorur() {
+        assert homePage.aramayaBaslamakIcinEnAz2KarakterYazmalisinizTextHome.getText().contains("en az 2 karakter girmelisiniz");
+    }
+}
