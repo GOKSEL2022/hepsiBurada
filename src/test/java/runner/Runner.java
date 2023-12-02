d92587d15d3d764b7371321f91c3788633eec8c4
Göksel Celik <gokselcelik5255@gmail.com>
2023-12-03 02:07:56 +0300

    03.12.2023

--- src/test/java/runner/Runner.java
+++ src/test/java/runner/Runner.java
@@ -15,7 +15,7 @@ import org.junit.runner.RunWith;
         monochrome = true,//raporlarin consoleda okunakli sekilde cikmasi icin
         features="src/test/resources/features",
         glue= {"stepdefinitions", "hooks"},
-        tags="@invalid_email"  ,
+        tags="@arama"  ,
         dryRun= false
 )
 public class Runner {
