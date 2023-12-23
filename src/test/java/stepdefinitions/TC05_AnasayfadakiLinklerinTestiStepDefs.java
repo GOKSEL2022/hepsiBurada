c6402a07da7197a7bc006774b67fbbffb90a21f7
Göksel Celik <gokselcelik5255@gmail.com>
2023-12-24 00:21:25 +0300

    24.12.2023

---
+++ src/test/java/stepdefinitions/TC05_AnasayfadakiLinklerinTestiStepDefs.java
@@ -0,0 +1,17 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.And;
+
+public class TC05_AnasayfadakiLinklerinTestiStepDefs {
+    @And("kullanici arama alaninin altinda elektronik sekmesinin uzerine gelir")
+    public void kullaniciAramaAlanininAltindaElektronikSekmesininUzerineGelir() {
+    }
+
+    @And("kullanici bilgisayar_tablet secenegine tiklar")
+    public void kullaniciBilgisayar_tabletSecenegineTiklar() {
+    }
+
+    @And("kullanici acilan sayfanin bilgisayar urunleri icerdigini dogrular")
+    public void kullaniciAcilanSayfaninBilgisayarUrunleriIcerdiginiDogrular() {
+    }
+}
