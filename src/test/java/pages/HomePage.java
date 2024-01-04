b1247bbb1c9a081f2049e97bc954a44fd49d7390
Göksel Celik <gokselcelik5255@gmail.com>
2024-01-05 00:08:27 +0300

    05.01.2024 konum alani test

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
+import java.util.List;
+
 public class HomePage {
     public HomePage(){
         PageFactory.initElements(Driver.getDriver(),this);
@@ -33,6 +35,36 @@ public class HomePage {
 
     @FindBy(xpath = "(//a[@href='https://www.hepsiburada.com/bilgisayarlar-c-2147483646'])[2]")
     public WebElement bilgisayarTabletSecenegiElektronikLinkHome;
+
+    @FindBy(xpath = "//*[@class='shipping-src-universal-partials-ShippingLocation-components-locationSelector-button-ShippingLocationSelectorButton__locationLabel']")
+    public WebElement konumLinkHome;
+
+    @FindBy(xpath = "(//*[@data-test-id='select-dropdown'])[1]")
+    public WebElement ilSecKonumLinkHome;
+
+    @FindBy(xpath = "(//*[.='İSTANBUL'])[2]")
+    public WebElement istanbulIlSec;
+
+    @FindBy(xpath = "(//*[@class='shipping-src-universal-partials-ShippingLocation-components-form-content-AddressSelector-style__label'])[2]")
+    public WebElement ilceSecKonumLinkHome;
+
+    @FindBy(xpath = "(//*[text()='ADALAR'])[2]")
+    public WebElement adalarIlceSec;
+
+    @FindBy(xpath = "//*[text()='Mahalle seçin']")
+    public WebElement mahalleSecKonumLinkHome;
+
+    @FindBy(xpath = "(//*[@data-test-id='option-item'])[1]")
+    public WebElement burgazadaMahalleSec;
+
+    @FindBy(xpath = "//*[@kind='primary']")
+    public WebElement kaydetButonKonumSecHome;
+
+    @FindBy(xpath = "//*[@class='hb-toast-text']")
+    public WebElement konumDegistirildiAlertHome;
+
+    @FindBy(xpath = "//*[@data-test-id='option-item']")
+    public List<WebElement> tumAdalarMahalleSec;
 
 
 
--- src/test/java/stepdefinitions/TC05_AnasayfadakiLinklerinTestiStepDefs.java
+++ src/test/java/stepdefinitions/TC05_AnasayfadakiLinklerinTestiStepDefs.java
@@ -1,12 +1,17 @@
 package stepdefinitions;
+import com.github.javafaker.Faker;
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import pages.AraPage;
 import pages.BilgisayarlarPage;
 import pages.HomePage;
 import utilities.Driver;
-import static utilities.ReusableMethods.clickWithTimeOut;
-import static utilities.ReusableMethods.scrollIntoViewJS;
+import java.util.List;
+import java.util.Random;
+import static utilities.ReusableMethods.*;
 
 public class TC05_AnasayfadakiLinklerinTestiStepDefs {
     AraPage araPage=new AraPage(); HomePage homePage=new HomePage();
@@ -25,4 +30,32 @@ public class TC05_AnasayfadakiLinklerinTestiStepDefs {
         scrollIntoViewJS(bilgisayarlarPage.bilgisayarFiyatlariVeModelleriTextBilgisayarlar);
         assert bilgisayarlarPage.bilgisayarFiyatlariVeModelleriTextBilgisayarlar.isDisplayed();
     }
-}
+    @And("kullanici konum sec linke tiklar")
+    public void kullaniciKonumSecLinkeTiklar() {
+        clickWithTimeOut(homePage.konumLinkHome,2);
+    }
+    @And("kullanici bir il secer")
+    public void kullaniciBirIlSecer() {
+        clickWithTimeOut(homePage.ilSecKonumLinkHome,2);
+        clickByJS(homePage.istanbulIlSec);
+    }
+    @And("kullanici bir ilce secer")
+    public void kullaniciBirIlceSecer() {
+        clickWithTimeOut(homePage.ilceSecKonumLinkHome,2);
+        clickByJS(homePage.adalarIlceSec);
+        }
+    @And("kullanici bir mahalle secer")
+    public void kullaniciBirMahalleSecer() {
+        clickByJS(homePage.mahalleSecKonumLinkHome);
+        clickByJS(homePage.burgazadaMahalleSec);
+        }
+    @And("kullanici kaydet butonuna tiklar")
+    public void kullaniciKaydetButonunaTiklar() {
+        clickWithTimeOut(homePage.kaydetButonKonumSecHome,2);
+    }
+    @When("kullanici konum degistirildi alerti goruntuler")
+    public void kullaniciKonumDegistirildiAlertiGoruntuler() {
+        assert homePage.konumDegistirildiAlertHome.isDisplayed();
+    }
+    }
+
