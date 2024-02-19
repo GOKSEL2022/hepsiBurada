61167419039c60320528d85a5db9f9e2553ff680
Göksel Celik <gokselcelik5255@gmail.com>
2024-02-20 02:20:51 +0300

    20.02.2024

---
+++ src/test/java/pages/EvTekstiliPage.java
@@ -0,0 +1,15 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class EvTekstiliPage {
+    public EvTekstiliPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+    @FindBy(xpath = "//*[@class='gQjF21piuOrwYvanqai8']")
+    public WebElement evTekstiliUrunleriTextEvTekstili;
+
+}
